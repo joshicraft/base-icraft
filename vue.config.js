4ddb0b207032fb9d7b4d895d21f32b260ada976d
josh scorrar <josh@icraft.co.nz>
2018-11-19 17:24:13 +1300

    no message

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
 const { VuetifyProgressiveModule } = require('vuetify-loader')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const loadMinified = require('./build/load-minified')
 module.exports = {
     devServer: {
         proxy: {
@@ -15,10 +20,32 @@ module.exports = {
     },
     configureWebpack: {
         plugins: [
-           new VuetifyLoaderPlugin(),
+            new VuetifyLoaderPlugin(),
             new PreloadWebpackPlugin({
                 rel: "preload",
                 include: "allChunks"
+            }),
+            // new HtmlWebpackPlugin({
+            //     filename: process.env.NODE_ENV === 'testing'
+            //         ? './public/index.html'
+            //         : './dist/index.html',
+            //     template: './public/index.html',
+            //     inject: true,
+            //     minify: {
+            //         removeComments: true,
+            //         collapseWhitespace: true,
+            //         removeAttributeQuotes: true
+            //         // more options:
+            //         // https://github.com/kangax/html-minifier#options-quick-reference
+            //     },
+            //     // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+            //     // chunksSortMode: 'dependency',
+            //     serviceWorkerLoader: `<script>${loadMinified(path.join(__dirname,
+            //         'build/service-worker-prod.js'))}</script>`
+            // }),
+            new CompressionWebpackPlugin({
+                algorithm: 'gzip',
+                test: /\.js(\?.*)?$/i
             })
             // new BundleAnalyzerPlugin()
         ]
@@ -29,6 +56,7 @@ module.exports = {
         //     .set('vuetify/lib', 'vuetify/es5/components');
         // config.plugin('vuetify-loader')
         //     .use(VuetifyLoaderPlugin);
+        // config.plugin('workbox')
         config.module
             .rule('vue')
             .use('vue-loader')
@@ -52,7 +80,7 @@ module.exports = {
             .loader('url-loader')
             .options({limit: 8000})
             .end()
-    }
+    },
     // css: {
     //     loaderOptions: {
     //         css: {
@@ -60,13 +88,19 @@ module.exports = {
     //         }
     //     }
     // }
-    // pwa: {
-    //     name: 'ICRAFT',
-    //     themeColor: '#4DBA87',
-    //     msTileColor: '#000000',
-    //     appleMobileWebAppCapable: 'yes',
-    //     appleMobileWebAppStatusBarStyle: 'black'
-    // },
+    pwa: {
+        name: 'ICRAFT',
+        themeColor: '#4DBA87',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            // swSrc is required in InjectManifest mode.
+            swSrc: './build/service-worker-prod.js',
+            // ...other Workbox options...
+        }
+    },
     // chainWebpack: config => {
     //     config.externals({
     //         'sharp': 'node_modules\\sharp\\vendor'
