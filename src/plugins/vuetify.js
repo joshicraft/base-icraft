a8c1524fe0c96425c8390ce72f86518977297f53
josh scorrar <josh@icraft.co.nz>
2018-11-20 15:48:45 +1300

    no message

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,7 @@ import Vuetify from 'vuetify/lib'
 import theme from './theme'
 import {Scroll} from 'vuetify/lib/directives'
 import 'vuetify/src/stylus/app.styl'
-// import '@mdi/font/css/materialdesignicons.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify, {
     theme,
