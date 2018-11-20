f82bd6b6ece79cf7ea0e3ed671c676105184ac0b
josh scorrar <josh@icraft.co.nz>
2018-11-20 23:49:27 +1300

    no message

--- src/components/custom/HomeSection.vue
+++ src/components/custom/HomeSection.vue
@@ -48,37 +48,41 @@
                 return this.loadTickerCount >= i
             },
             scrollH(){
-                let $e = this.$el
-                let $left = $e.querySelector('.wipe-left')
-                let $right = $e.querySelector('.wipe-right')
-                let $iris = $e.querySelector('.iris')
-                let $irisRings = [$iris.querySelector('.iris-1'), $iris.querySelector('.iris-2'), $iris.querySelector('.iris-3'), $iris.querySelector('.iris-4')]
+                let $e = this.$el,
+                 $left,
+                 $right,
+                 $iris,
+                 $title,
+                 $irisRings
                 let pos = $e.getBoundingClientRect().y
                 if (pos < window.innerHeight / 2.5 && !$e.scrollAnimated) {
-                    console.log('ss')
+                    $left = $e.querySelector('.wipe-left')
+                    $right = $e.querySelector('.wipe-right')
+                    $iris = $e.querySelector('.iris')
+                    $title =$e.querySelector('.title-a')
+                    $irisRings = [$iris.querySelector('.iris-1'), $iris.querySelector('.iris-2'), $iris.querySelector('.iris-3'), $iris.querySelector('.iris-4')]
+                    $e.scrollAnimated = true
                     new TimelineMax()
                         .to($iris, 0.3, {scale: 1, ease: Bounce.easeOut})
                         .to($iris.querySelector('.iris-1'), 0.3, {rotation: 360, transformOrigin: '50% 50%'}, '-=' + .15)
                         .to($iris.querySelector('.iris-2'), 0.3, {rotation: 180, transformOrigin: '50% 50%'}, '-=' + .15)
                         .to($iris.querySelector('.iris-3'), 0.3, {rotation: -180, transformOrigin: '50% 50%'}, '-=' + .15)
                         .to($iris.querySelector('.iris-4'), 0.3, {rotation: 360, transformOrigin: '50% 50%'}, '-=' + .15)
-                        .fromTo($e.querySelector('.title-a'), 1.2, {y: 60, autoAlpha: 0}, {y: 0, autoAlpha: 1}, '-=0.5')
-                        .to($iris, 0.3, {scale: 0, ease: Bounce.easeOut})
-                        .to($left ? $left : $right, 0.5, {x: $left ? '-100%' : '100%'})
-
-                    // $left.classList.add('left-out')
-                    // $right.classList.add('right-out')
-                    // $iris.classList.add('iris-animate')
-                    $e.scrollAnimated = true
-                } else if ($e.scrollAnimated && pos > window.innerHeight - 300) {
+                        .fromTo($title, 1.2, {y: 60, autoAlpha: 0}, {y: 0, autoAlpha: 1}, '-=0.7')
+                        .to($iris, 0.3, {scale: 0, ease: Bounce.easeOut}, '-=0.5')
+                        .to($left ? $left : $right, 0.5, {x: $left ? '-100%' : '100%'}, '-=0.2')
+                } else if ($e.scrollAnimated && pos > window.innerHeight - 2) {
                     $e.scrollAnimated = false
+                    $left = $e.querySelector('.wipe-left')
+                    $right = $e.querySelector('.wipe-right')
+                    $iris = $e.querySelector('.iris')
+                    $title =$e.querySelector('.title-a')
+                    $irisRings = [$iris.querySelector('.iris-1'), $iris.querySelector('.iris-2'), $iris.querySelector('.iris-3'), $iris.querySelector('.iris-4')]
                     new TimelineMax()
-                        .from($iris, 0.2, {scale: 1, ease: Bounce.easeOut})
-                        .to($irisRings, 0.2, {rotation: 0})
-                        .to($left ? $left : $right, 0.3, {x: '0%'})
-                    // $left.classList.remove('left-out')
-                    // $right.classList.remove('right-out')
-                    // $iris.classList.remove('iris-animate')
+                        .set($iris,  {scale: 1})
+                        .set($irisRings, {rotation: 0})
+                        .set($left ? $left : $right, {x: '0%'})
+                        .set($e.querySelector('.title-a'), {y: 60, autoAlpha: 0})
                 }
             }
         }
