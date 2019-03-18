58c09f7c49d3229284c8ca9732fedbab9c5a191b
icraft-nz <josh@icraft.co.nz>
2019-03-18 20:18:13 +1300

    dev branch setup

--- src/main.js
+++ src/main.js
@@ -108,7 +108,7 @@ Vue.mixin({
                     useH = this.$el || img
                     h = this.$el ? this.$el : img
                     console.log(h)
-                    h = h.getBoundingClientRect ? h.getBoundingClientRect().height : sizes[0]
+                    h = h ? h.getBoundingClientRect ? h.getBoundingClientRect().height : sizes[0] : sizes[0]
                     path = '/dist' + path
                     return path + sizes[0] + '-' + name + this.ext(ext || 'jpg') + '?nf_resize=fit&h=' + h
                 }
