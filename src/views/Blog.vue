39ba538d723d0f050a0e4e910ee779f2c794e65f
icraft-nz <josh@icraft.co.nz>
2019-03-19 16:51:59 +1300

    cms implemented

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -11,15 +11,8 @@
 <script>
 
     export default {
-        data: () => {
-            return {
-                lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`,
-
-            }
-        },
         computed: {
             items () {
-                console.log(Object.values(this.$t('Blog')))
                 return Object.values(this.$t('Blog'))
             }
         }
