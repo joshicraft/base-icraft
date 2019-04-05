9f77e716e1e80a4a79cacb6dd8f2add4adfb54b8
icraft-nz <josh@icraft.co.nz>
2019-04-06 10:11:46 +1300

    faq started

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
                 v-for="(item, i) in items"
                 :key="i"
         >
-            <home-section v-if="loadPoint(i-1)"
+            <home-section v-if="loadPoint(i-100)"
                           :item="item" :i="i"></home-section>
 
         </v-layout>
