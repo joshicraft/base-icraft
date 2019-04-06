99b75c19be18444dfcb5f2f57816c2538a97fa46
icraft-nz <josh@icraft.co.nz>
2019-04-06 17:23:07 +1300

    faq started

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="">
         <template
-            v-for="(item, i) in items"
+                v-for="(item, i) in blogs"
         >
             <custom-blog-preview :index="i" :item="item"></custom-blog-preview>
         </template>
@@ -11,9 +11,42 @@
 <script>
 
     export default {
-        computed: {
-            items () {
-                return Object.values(this.$t('Blog'))
+        data() {
+            return {
+                blogRoutes: [],
+                blogs: [],
+                blogLoadIndex: 0
+            }
+        },
+        mounted() {
+            this.blogRoutes = this.filterBlogs()
+            this.getBlogs()
+        },
+        methods: {
+            filterBlogs() {
+                return this.$router.options.routes.filter(route => {
+                    return route.path.search('/blog/') !== -1
+                })
+            },
+            getBlogs() {
+                let $this = this
+                fetch(window.location.origin + "/src/lang/en/Blog" + this.blogRoutes[this.blogLoadIndex].path + ".json")
+                    .then(res => {
+                        if (res.status !== 200) {
+                            console.log('Looks like there was a problem. Status Code: ' +
+                                res.status);
+                            return;
+                        }
+
+                        // Examine the text in the response
+                        res.json().then(function (data) {
+                            $this.blogs.push(data)
+                            console.log(data);
+                        });
+                    })
+                    .catch(err => {
+                        console.log('Fetch Error :-S', err);
+                    })
             }
         }
     }
